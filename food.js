f03311952ca984284817dd2b7a6b110d0bf089ef
qasrina26 <safileaqasrina@gmail.com>
2024-08-26 16:26:17 +0800

    Update food.js

--- food.js
+++ food.js
@@ -1,27 +1,27 @@
-import{ onSnake, expandSnake } from './snake.js'
-import{ randomGridPosition } from './grid.js'
+import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
 
-Let food = getRendomFoodPosition()
+let food = getRandomFoodPosition()
 const EXPANSION_RATE = 5
 
 export function update() {
   if (onSnake(food)) {
-    expandsnake(EXPANTION_RATE)
+    expandSnake(EXPANSION_RATE)
     food = getRandomFoodPosition()
   }
 }
 
-export function  draw(gameBoard) {
+export function draw(gameBoard) {
   const foodElement = document.createElement('div')
   foodElement.style.gridRowStart = food.y
   foodElement.style.gridColumnStart = food.x
-  foodElement.classlist.add('food')
+  foodElement.classList.add('food')
   gameBoard.appendChild(foodElement)
 }
 
-function getRandomPosition() {
-  Let newFoodPosition
-  while (newFoodPosition ==nu11 || onSnake(newFoodPosition)) {
+function getRandomFoodPosition() {
+  let newFoodPosition
+  while (newFoodPosition == null || onSnake(newFoodPosition)) {
     newFoodPosition = randomGridPosition()
   }
   return newFoodPosition
